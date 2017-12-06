3252fb8c1e5f03983512bee0f490ca154e82559a
Marcel Eschmann <marcel.eschmann@gmail.com>
2017-12-06 18:51:07 +0100

    Map heap to physical pages

--- src/lib.rs
+++ src/lib.rs
@@ -21,54 +21,33 @@ extern crate x86_64;
 extern crate alloc;
 
 #[macro_use]
+extern crate once;
+
+#[macro_use]
 mod vga_buffer;
 mod fib;
 mod memory;
 
 #[no_mangle]
 pub extern "C" fn rust_main(multiboot_information_address: usize) {
-    use memory::FrameAllocator;
     // ATTENTION: we have a very small stack and no guard page
-
-    // the same as before
     vga_buffer::clear_screen();
     println!("Hello World{}", "!");
 
     let boot_info = unsafe {
         multiboot2::load(multiboot_information_address)
     };
-    let memory_map_tag = boot_info.memory_map_tag()
-        .expect("Memory map tag required");
-    let elf_sections_tag = boot_info.elf_sections_tag()
-        .expect("Elf sections tag required");
-
-    let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
-        .min().unwrap();
-    let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size)
-        .max().unwrap();
-
-    let multiboot_start = multiboot_information_address;
-    let multiboot_end = multiboot_start + (boot_info.total_size as usize);
-
-    println!("kernel start: 0x{:x}, kernel end: 0x{:x}",
-        kernel_start, kernel_end);
-    println!("multiboot start: 0x{:x}, multiboot end: 0x{:x}",
-        multiboot_start, multiboot_end);
-
-    let mut frame_allocator = memory::AreaFrameAllocator::new(
-        kernel_start as usize, kernel_end as usize, multiboot_start,
-        multiboot_end, memory_map_tag.memory_areas());
 
     enable_nxe_bit();
     enable_write_protect_bit();
-    memory::remap_the_kernel(&mut frame_allocator, boot_info);
 
-    frame_allocator.allocate_frame();
-
-    println!("It did not crash!");
+    // set up guard page and map the heap pages
+    memory::init(boot_info);
 
     use alloc::boxed::Box;
     let heap_test = Box::new(42);
+
+    println!("It did not crash!");
 
     loop {}
 }
